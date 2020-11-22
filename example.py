b66c5b477b4c4f987f5bcfd0adf3a4cd9cf75608
DK <Dikshyant99@gmail.com>
2020-11-22 12:18:42 -0600

    Finishined cloze test

--- example.py
+++ example.py
@@ -28,27 +28,50 @@ def gothroughdeps(deps,option):
         if x[0] != option[0]:
             information = table.pmi(x[0],x[1],option[0],option[1])
             result.append(information)
+    
     return result
 
 # load the pre-built model
 with open("all.json") as fp:
     table = chains.ProbabilityTable(json.load(fp))
 
+
+#this is my cloze test  (system passes has 45% success rate approx.)
 # load testing data
-test = chains.load_data("oop.csv")
+test = chains.load_data("val.csv")
+correct=0
+total=0
 for t in test:
     one, two = parse_test_instance(t)
-    # one_deps = chains.extract_dependency_pairs(one)
     prot=chains.protagonist(one)
     prot2=chains.protagonist(two)
+
+    #in case prot is not found
+    try:
+        testing=prot[1]
+        testing2=prot2[1]
+    except TypeError as ex:
+        total+=1
+        continue
+
     one_deps = chains.coreferring_pairs(one,prot[1].root)
-    print (one_deps)
     two_deps = chains.coreferring_pairs(two,prot2[1].root)
+    
+    #in case my system cant parse the dependecy pairs properly (fail the line and move on)
+    if len(one_deps)==0 or len (two_deps)==0:
+        total+=19
+        continue
+    
     oneoption = one_deps[-1]
     twooption = two_deps[-1]
     oneresult=gothroughdeps(one_deps,oneoption)
     tworesult=gothroughdeps(two_deps,twooption)
-    print (oneresult)
+    
+    #in case my system cant parse the dependecy pairs properly (fail the line and move on)  
+    if len(oneresult)==0 or len (tworesult)==0:
+        total+=1
+        continue
+    
     
     ''' my logic is that i get X amount of info by relating each of the verbs with the optional verb
         and to choose between option one and two i just need to find the average of how much information I get from each
@@ -56,19 +79,21 @@ for t in test:
     '''
     # got this code from     https://careerkarma.com/blog/python-average/#:~:text=We%20can%20find%20the%20average,the%20Python%20mean()%20function.
     # average = sum(orders) / len(orders)
-    #oneaverage = sum(oneresult) / len(oneresult)
-    #twoaverage = sum(tworesult) / len(tworesult)
+    oneaverage = sum(oneresult) / len(oneresult)
+    twoaverage = sum(tworesult) / len(tworesult)
 
-    # if oneaverage>twoaverage:
-    #     result = 1
-    # else:
-    #     result = 2
+    if oneaverage>twoaverage:
+        result = 1
+    else:
+        result = 2
 
-    # print (result)
-   # pprint(one[2:])
-    #pprint(two[2:])
-    
-    #pprint("answer:"+ str(story_answer(t)))
+
+
+    if result == story_answer(t)[1]:
+        correct+=1
+    total+=1
+
+print ("Out of {} trials {} were succesful with {} success rate".format(total,correct,correct/total*100))
 
 
 
